44bb1ba859cbc4d404d20b99345a64a358acbf34
Daniel <danielmesquitadev@gmail.com>
2026-01-10 19:05:28 -0300

    feat: first commit

---
+++ src/main/java/dev/danielmesquita/miniauthorizer/MiniAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package dev.danielmesquita.miniauthorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAuthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAuthorizerApplication.class, args);
+	}
+
+}
