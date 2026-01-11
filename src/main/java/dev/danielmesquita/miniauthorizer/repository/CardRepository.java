8a89f5494fc3600a14a4a6e00ad746d1c8ce1cee
Daniel <danielmesquitadev@gmail.com>
2026-01-11 01:15:29 -0300

    feat: create card service with lock option for operation

--- src/main/java/dev/danielmesquita/miniauthorizer/repository/CardRepository.java
+++ src/main/java/dev/danielmesquita/miniauthorizer/repository/CardRepository.java
@@ -1,8 +1,18 @@
 package dev.danielmesquita.miniauthorizer.repository;
 
 import dev.danielmesquita.miniauthorizer.entity.Card;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CardRepository extends JpaRepository<Card, Long> {}
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query("SELECT c FROM Card c WHERE c.cardNumber = :number")
+  Optional<Card> findByCardNumberForUpdate(String number);
+}
---
+++ src/main/java/dev/danielmesquita/miniauthorizer/service/CardService.java
@@ -0,0 +1,35 @@
+package dev.danielmesquita.miniauthorizer.service;
+
+import dev.danielmesquita.miniauthorizer.entity.Card;
+import dev.danielmesquita.miniauthorizer.repository.CardRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+public class CardService {
+
+  private final CardRepository repository;
+
+  public CardService(CardRepository repository) {
+    this.repository = repository;
+  }
+
+  @Transactional
+  public void executeOperation(String cardNumber, String password, BigDecimal value) {
+    Card card = repository.findByCardNumberForUpdate(cardNumber)
+            .orElseThrow(() -> new IllegalArgumentException("Card not found with number: " + cardNumber));
+
+    if (!card.getPassword().equals(password)) {
+      throw new IllegalArgumentException("Invalid card password.");
+    }
+
+    if (card.getBalance().compareTo(value) < 0) {
+      throw new IllegalArgumentException("Insufficient balance.");
+    }
+
+    card.setBalance(card.getBalance().subtract(value));
+    repository.save(card);
+  }
+}
