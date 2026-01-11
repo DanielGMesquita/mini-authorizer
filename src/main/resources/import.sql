3a865c20b3b721e0b269388e0903ded9d4900ec1
Daniel <danielmesquitadev@gmail.com>
2026-01-11 02:03:15 -0300

    feat: initial user and role logic

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,11 @@
 INSERT INTO tb_card (card_number, card_holder_name, password, balance) VALUES ('1234', 'Daniel Mesquita', 'pass', 90.85);
 
-INSERT INTO tb_user (username, password, role) VALUES ('daniel', 'pass', 'ROLE_USER');
-INSERT INTO tb_user (username, password, role) VALUES ('admin', 'adminpass', 'ROLE_ADMIN');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Brown', 'maria@gmail.com', '$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Green', 'alex@gmail.com', '$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
