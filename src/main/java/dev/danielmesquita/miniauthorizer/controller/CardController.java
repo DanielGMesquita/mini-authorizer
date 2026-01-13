d7bc7966b4544a095f7c896239896f032691b5c9
Daniel <danielmesquitadev@gmail.com>
2026-01-13 00:31:57 -0300

    fix: createCard now uses cardNumber

--- src/main/java/dev/danielmesquita/miniauthorizer/controller/CardController.java
+++ src/main/java/dev/danielmesquita/miniauthorizer/controller/CardController.java
@@ -3,12 +3,11 @@ package dev.danielmesquita.miniauthorizer.controller;
 import dev.danielmesquita.miniauthorizer.dto.CardDTO;
 import dev.danielmesquita.miniauthorizer.service.CardService;
 import jakarta.validation.Valid;
+import java.math.BigDecimal;
+import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.math.BigDecimal;
-import java.net.URI;
 
 @RestController
 @RequestMapping("/cards")
@@ -21,16 +20,17 @@ public class CardController {
   }
 
   @GetMapping("/{cardNumber}")
-  public ResponseEntity<BigDecimal> balance (@PathVariable String cardNumber) {
+  public ResponseEntity<BigDecimal> balance(@PathVariable String cardNumber) {
     return ResponseEntity.ok(cardService.getBalance(cardNumber));
   }
 
   @PostMapping
   public ResponseEntity<CardDTO> createCard(@Valid @RequestBody CardDTO cardDTO) {
     cardDTO = cardService.createCard(cardDTO);
-    URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-            .path("/{id}")
-            .buildAndExpand(cardDTO.getId())
+    URI uri =
+        ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{cardNumber}")
+            .buildAndExpand(cardDTO.getCardNumber())
             .toUri();
     return ResponseEntity.created(uri).body(cardDTO);
   }
