0550600bba17a868eadc689611b24331ee18508f
Daniel <danielmesquitadev@gmail.com>
2026-01-13 00:26:54 -0300

    refactor: Use only one SecurityConfig file

--- src/main/java/dev/danielmesquita/miniauthorizer/config/SecurityConfig.java
+++ src/main/java/dev/danielmesquita/miniauthorizer/config/SecurityConfig.java
@@ -2,13 +2,28 @@ package dev.danielmesquita.miniauthorizer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  @Order(2)
+  @Profile("test")
+  public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    return http.build();
   }
 }
